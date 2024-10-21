24756a57ca3d648a13f3949648d25d7e8f8e67fb
seifeddineelbehi <elbehiseifeddine@gmail.com>
2024-10-21 21:35:55 +0100

    new fixes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,5 @@
-import {NextIntlClientProvider} from 'next-intl';
-import {getLocale, getMessages} from 'next-intl/server';
- 
-export default async function RootLayout({
-  children
-}) {
-  const locale = await getLocale();
- 
-  // Providing all messages to the client
-  // side is the easiest way to get started
-  const messages = await getMessages();
- 
-  return ( 
-    <html lang={locale}>
-      <body>
-        <NextIntlClientProvider messages={messages}>
-          {children}
-        </NextIntlClientProvider>
-      </body>
-    </html>
-  );
-}
+
+export default function RootLayout({ children }) {
+  return (children
+  )
+} 
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,11 @@ import createMiddleware from 'next-intl/middleware';
  
 export default createMiddleware({
     locales: ['en','de','fr'],
-    defaultLocale: 'en'
+    defaultLocale: 'en',
+    localePrefix: 'as-needed'
 });
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/talent', '/employers', '/terms','/privacy','/accessibility','/(de|en|fr)/:path*','/((?!api_next|_vercel|.*\\..*).*)'] 
-}; 
+  matcher: ['https://storklink.de/', '/talent', '/employers', '/terms','/privacy','/accessibility','/(de|en|fr)/:path*','/((?!_next|_vercel|.*\\..*).*)'] 
+};  
