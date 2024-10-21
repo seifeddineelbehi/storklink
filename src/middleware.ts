538838cea22fdbba4550735c838a57d033b5197b
seifeddineelbehi <elbehiseifeddine@gmail.com>
2024-10-21 21:08:24 +0100

    new fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,10 @@ import createMiddleware from 'next-intl/middleware';
  
 export default createMiddleware({
     locales: ['en','de','fr'],
-    defaultLocale: 'en',
-    localePrefix: 'as-needed'
+    defaultLocale: 'en'
 });
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/talent', '/employers', '/terms','/privacy','/accessibility','/(de|en|fr)/:path*','/((?!_next|_vercel|_netlify|storklink.de|.*\\..*).*)'] 
+  matcher: ['/', '/talent', '/employers', '/terms','/privacy','/accessibility','/(de|en|fr)/:path*','/((?!_next|_vercel|.*\\..*).*)'] 
 }; 
