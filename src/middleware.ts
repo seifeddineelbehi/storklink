a45b4512901f491c86967e5f6f045dfc787b9fc1
seifeddineelbehi <elbehiseifeddine@gmail.com>
2024-10-22 16:53:25 +0100

    new fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,9 @@
 import createMiddleware from 'next-intl/middleware';
-import {locales, localePrefix, pathnames} from './navigation';
 
 export default createMiddleware({
     locales: ['en','de','fr'],
     defaultLocale: 'en',
     localePrefix: 'as-needed',
-    pathnames
 });
  
 export const config = {
