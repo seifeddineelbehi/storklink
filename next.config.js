f7fade22eedc0838c212dd347483ddec7c6b20ad
seifeddineelbehi <elbehiseifeddine@gmail.com>
2024-10-23 21:29:47 +0100

    new fixes

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   images: {
     unoptimized: true,
   },
